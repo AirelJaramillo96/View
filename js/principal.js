5b66d4e4be1fb46063e0b344de601fb88ecdce8a
AirelJaramillo96 <airel.jaramillo@cceo.com.mx>
2019-07-16 12:52:28 -0500

    Create tables average

---
+++ js/principal.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app', /*identificador */
+    data : { /*variables*/
+        lista : [
+            {nombre: 'Pedro Lopez', promedio: '10', estado: false},
+            {nombre: 'Angel Garcia', promedio: '17', estado: true},
+            {nombre: 'Joel Diaz', promedio: '15', estado: true},
+        ]
+    }
+})
