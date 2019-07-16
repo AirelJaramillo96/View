f73d2e1a36c9de58dae0aa2534789f26ce32c3c5
AirelJaramillo96 <airel.jaramillo@cceo.com.mx>
2019-07-16 13:25:30 -0500

    Finishied Table Students

--- js/principal.js
+++ js/principal.js
@@ -2,9 +2,30 @@ var app = new Vue({
     el: '#app', /*identificador */
     data : { /*variables*/
         lista : [
-            {nombre: 'Pedro Lopez', promedio: '10', estado: false},
-            {nombre: 'Angel Garcia', promedio: '17', estado: true},
-            {nombre: 'Joel Diaz', promedio: '15', estado: true},
-        ]
-    }
+            
+        ],
+        nombre: '',
+        promedio: '',
+
+    },
+    methods: {
+        agregarNota: function (){ /*funcion para guardar los datos de los promedios si el promedio, que 
+            recogimos en el index es mayor a 12.5 entonces esta aprobado. Esta funcion se activa al presionar
+            el boton "Guardar" */
+            estadoA=false;
+            if(this.promedio>=12.5){
+                estadoA=true;
+            }
+            if(this.nombre!="" && this.promedio!=""){ /**Proceso en el que se guardan los valores en la lista
+                 */
+                this.lista.push({nombre:this.nombre, promedio:this.promedio, estado:estadoA});
+                this.nombre="";
+                this.promedio="";
+            }
+            else{
+                alert("Ingrese el nombre y promedio del estudiante") /*Comprobamos que no haya espacios en 
+                blanco */
+            }
+        }
+    },
 })
